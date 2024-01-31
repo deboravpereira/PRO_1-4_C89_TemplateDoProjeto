f30c109562d9b4b1bf68b7f7481937e983d5013e
deboravpereira <100171504+deboravpereira@users.noreply.github.com>
2024-01-31 14:59:42 -0300

    Update StarMap.js

--- screens/StarMap.js
+++ screens/StarMap.js
@@ -1,17 +1,64 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, StyleSheet, Platform, StatusBar, SafeAreaView, ImageBackgrounda, ImageBackground } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default class StarMapScreen extends Component {
+    constructor() {
+        super()
+        this.state = {
+            longitude: '',
+            latitude: ''
+        }
+    }
     render() {
+        {/* Declare states */}
+
+        const path = `https://virtualsky.lco.global/embed/index.html?longitude=${longitude}&latitude=${latitude}&constellations=true&constellationlabels=true&showstarlabels=true&gridlines_az=true&live=true&projection=stereo&showdate=false&showposition=false`
         return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                <Text>Tela do mapa estelar!</Text>
+            <View style={{ flex: 1, backgroundColor: "#1a0023" }}>
+                <SafeAreaView style={styles.droidSafeArea} />
+                <View style={{ flex: 0.3, marginTop: 20, alignItems: 'center' }}>
+                    <Text style={styles.titleText}>Mapa Estelar</Text>
+                    {/*INSIRA TEXT INPUT PARA LONGITUDE*/}
+
+                    {/*INSIRA TEXT INPUT PARA LATITUDE*/}
+                </View>
+                {/*INSIRA WEBVIEW*/}
             </View>
-        )
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    titleText: {
+        fontSize: 35,
+        fontWeight: "bold",
+        color: "white",
+        justifyContent: "center",
+        alignContent: "center",
+    },
+    inputStyle: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 20,
+        marginTop: 20,
+        marginRight: 20,
+        marginLeft: 20,
+        textAlign: 'center',
+        color: 'white',
+        width: 200
+    }
+})
